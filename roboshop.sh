fa80898cf51b44b31e453819459f304da453b326
p-ismail <ismailsmile7337@gmail.com>
2026-02-02 21:24:54 +0530

    Roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-     Instance_Id = $(aws ec2 run-instances \
+     INSTANCE_ID=$(aws ec2 run-instances \
      --image-id $AMI_ID \
      --instance-type t3.micro \
      --security-group-ids $SG_ID \
@@ -16,17 +16,19 @@ do
      if [ $instance == "frontend" ]; then
           IP=$(
              aws ec2 describe-instances \
-             --instance-ids $Instance_Id \
+             --instance-ids $INSTANCE_ID \
              --query 'Reservations[*].Instances[*].PublicIpAddress' \
             --output text
           )
     else
             IP=$(
              aws ec2 describe-instances \
-             --instance-ids $Instance_Id \
+             --instance-ids $INSTANCE_ID \
              --query 'Reservations[*].Instances[*].PrivateIpAddress' \
             --output text
         )
+
+        echo "IP Address is $IP"
     fi
 
 done
