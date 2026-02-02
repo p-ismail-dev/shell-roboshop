71fc120fc06264faac79c29ac415a4c7c46ad56f
p-ismail <ismailsmile7337@gmail.com>
2026-02-02 21:17:14 +0530

    Roboshop

---
+++ roboshop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+SG_ID="sg-02e07349658ce6d66"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+     Instance_Id = $(aws ec2 run-instances \
+     --image-id $AMI_ID \
+     --instance-type t3.micro \
+     --security-group-ids $SG_ID \
+     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+     --query 'Instances[0].InstanceId'\
+     --output text)
+
+     if [ $instance == "frontend" ]; then
+          IP=$(
+             aws ec2 describe-instances \
+             --instance-ids $Instance_Id \
+             --query 'Reservations[*].Instances[*].PublicIpAddress' \
+            --output text
+          )
+    else
+            IP=$(
+             aws ec2 describe-instances \
+             --instance-ids $Instance_Id \
+             --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+            --output text
+        )
+    fi
+
+done
